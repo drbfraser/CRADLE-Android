59d623be8175ee10db99856b7428e3009776dddb
sachin <sraturi@sfu.ca>
2019-10-04 11:55:36 -0700

    splash screen opens intro activity after half a second

--- app/src/main/java/com/cradletrial/cradlevhtapp/view/SplashActivity.java
+++ app/src/main/java/com/cradletrial/cradlevhtapp/view/SplashActivity.java
@@ -1,5 +1,7 @@
 package com.cradletrial.cradlevhtapp.view;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +9,18 @@ import com.cradletrial.cradlevhtapp.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private final int DELAY = 500;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, IntroActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, DELAY);
     }
 }
