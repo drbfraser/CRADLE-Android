943342a5c770428a87c1746a0cc278a553d765fc
sachin <sraturi@sfu.ca>
2019-10-04 22:08:19 -0700

    toolbar logo setup etc

--- app/src/main/java/com/cradletrial/cradlevhtapp/view/DashBoardActivity.java
+++ app/src/main/java/com/cradletrial/cradlevhtapp/view/DashBoardActivity.java
@@ -4,9 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.cradletrial.cradlevhtapp.R;
+import com.cradletrial.cradlevhtapp.view.ui.settings.SettingsActivity;
+
+import static com.cradletrial.cradlevhtapp.view.TabActivityBase.TAB_ACTIVITY_BASE_SETTINGS_DONE;
 
 public class DashBoardActivity extends AppCompatActivity  implements View.OnClickListener {
 
@@ -19,6 +24,9 @@ public class DashBoardActivity extends AppCompatActivity  implements View.OnClic
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
         setupOnClickListner();
+        //getSupportActionBar().setDisplayShowHomeEnabled(true);
+        //getSupportActionBar().setDisplayUseLogoEnabled(true);
+        //getSupportActionBar().setLogo(getResources().getDrawable(R.drawable.upload));
 
     }
     private void setupOnClickListner(){
@@ -30,6 +38,28 @@ public class DashBoardActivity extends AppCompatActivity  implements View.OnClic
         helpCard.setOnClickListener(this::onClick);
         statCard.setOnClickListener(this);
         referralCard.setOnClickListener(this::onClick);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            Intent intent = SettingsActivity.makeLaunchIntent(this);
+            startActivityForResult(intent, TAB_ACTIVITY_BASE_SETTINGS_DONE);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
