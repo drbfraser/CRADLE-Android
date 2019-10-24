2664a6ff98a20e27a386b5e4ede43d2de15722a7
sachin <sraturi@sfu.ca>
2019-10-24 16:53:17 -0700

    login page launches intro activity minor change to UI

--- app/src/main/java/com/cradletrial/cradlevhtapp/view/ui/LoginActivity.java
+++ app/src/main/java/com/cradletrial/cradlevhtapp/view/ui/LoginActivity.java
@@ -1,9 +1,15 @@
 package com.cradletrial.cradlevhtapp.view.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.cradletrial.cradlevhtapp.R;
+import com.cradletrial.cradlevhtapp.view.IntroActivity;
+import com.cradletrial.cradlevhtapp.view.SplashActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,5 +17,19 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        setupLogin();
+    }
+
+    private void setupLogin() {
+        TextView loginbuttoon = findViewById(R.id.loginButton);
+        loginbuttoon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(LoginActivity.this, IntroActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
