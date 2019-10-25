36bd458ff3d98e77e25c2d385fac163f756267b7
sachin <sraturi@sfu.ca>
2019-10-24 17:43:11 -0700

    setup login button , update UI to flexible for other app

--- app/src/main/java/com/cradletrial/cradlevhtapp/view/ui/LoginActivity.java
+++ app/src/main/java/com/cradletrial/cradlevhtapp/view/ui/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -17,19 +18,19 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-       // setupLogin();
+        setupLogin();
     }
 
-//    private void setupLogin() {
-//        TextView loginbuttoon = findViewById(R.id.loginButton);
-//        loginbuttoon.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                //todo change it so that we authenticate the user first and than go to the intro page.
-//                Intent intent = new Intent(LoginActivity.this, IntroActivity.class);
-//                startActivity(intent);
-//                finish();
-//            }
-//        });
-//    }
+    private void setupLogin() {
+        Button loginbuttoon = findViewById(R.id.loginButton);
+        loginbuttoon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //todo change it so that we authenticate the user first and than go to the intro page.
+                Intent intent = new Intent(LoginActivity.this, IntroActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
 }
