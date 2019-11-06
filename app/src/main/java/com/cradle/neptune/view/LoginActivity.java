479a84da872ae0d595ae921ca4d9b9432a149fbd
sachin <sraturi@sfu.ca>
2019-11-05 21:43:49 -0800

    cleanup

--- app/src/main/java/com/cradle/neptune/view/LoginActivity.java
+++ app/src/main/java/com/cradle/neptune/view/LoginActivity.java
@@ -37,8 +37,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Log.d("buggg","he1111rerer");
-
         checkSharedPrefForLogin();
         setupLogin();
     }
