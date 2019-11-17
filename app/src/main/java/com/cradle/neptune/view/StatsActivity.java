1cbf9221e36ad31e6ef3af5815e4345759ba7d91
sachin <sraturi@sfu.ca>
2019-11-17 00:11:58 -0800

    setup sample bar graph

--- app/src/main/java/com/cradle/neptune/view/StatsActivity.java
+++ app/src/main/java/com/cradle/neptune/view/StatsActivity.java
@@ -5,7 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.cradle.neptune.R;
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
@@ -20,7 +25,45 @@ public class StatsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
 
-        setupLineChar();
+        //setupLineChar();
+        setupBarChart();
+    }
+
+    private void setupBarChart() {
+        BarChart barChart = findViewById(R.id.bargraph);
+
+        List<BarEntry> entries = new ArrayList<>();
+        List<BarEntry> entries1 = new ArrayList<>();
+        List<BarEntry> entries2 = new ArrayList<>();
+
+        entries.add(new BarEntry(3,22));
+        entries1.add(new BarEntry(1,18));
+        entries2.add(new BarEntry(2,8));
+
+        BarDataSet dataSet = new BarDataSet(entries,"HeartRate1");
+        BarDataSet dataSet1 = new BarDataSet(entries1,"HeartRate2");
+        BarDataSet dataSet2 = new BarDataSet(entries2,"HeartRate3");
+
+        dataSet.setColor(Color.GREEN);
+        dataSet1.setColor(Color.RED);
+        dataSet2.setColor(Color.YELLOW);
+
+
+        barChart.setDrawBorders(false);
+        barChart.setDrawGridBackground(false);
+        barChart.getAxisRight().setDrawLabels(false);
+        barChart.getXAxis().setDrawAxisLine(false);
+        barChart.getAxisRight().setDrawGridLines(false);
+        barChart.getXAxis().setDrawGridLines(false);
+        barChart.getAxisLeft().setDrawGridLines(false);
+        barChart.getXAxis().setDrawLabels(false);
+        barChart.getAxisRight().setAxisMinimum(0);
+        barChart.getAxisLeft().setAxisMinimum(0);
+
+        BarData lineData = new BarData(dataSet,dataSet1,dataSet2);
+
+        barChart.setData(lineData);
+        barChart.invalidate();
     }
 
     private void setupLineChar() {
