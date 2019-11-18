52f641c640351507e4a2e7aeb4522e20f0630f2f
sachin <sraturi@sfu.ca>
2019-11-18 12:05:04 -0800

    Line chart is a smooth curve now

--- app/src/main/java/com/cradle/neptune/view/StatsActivity.java
+++ app/src/main/java/com/cradle/neptune/view/StatsActivity.java
@@ -3,11 +3,14 @@ package com.cradle.neptune.view;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.cradle.neptune.R;
+import com.cradle.neptune.dagger.MyApp;
+import com.cradle.neptune.model.Reading;
+import com.cradle.neptune.model.ReadingManager;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
@@ -18,20 +21,26 @@ import com.github.mikephil.charting.data.LineDataSet;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.inject.Inject;
+
 public class StatsActivity extends AppCompatActivity {
 
+    @Inject
+    ReadingManager readingManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
+        ((MyApp) getApplication()).getAppComponent().inject(this);
 
-        //setupLineChar();
+        setupLineChar();
         setupBarChart();
     }
 
     private void setupBarChart() {
-        BarChart barChart = findViewById(R.id.bargraph);
 
+        List<Reading> readings = readingManager.getReadings(this);
+        BarChart barChart = findViewById(R.id.bargraph);
         List<BarEntry> entries = new ArrayList<>();
         List<BarEntry> entries1 = new ArrayList<>();
         List<BarEntry> entries2 = new ArrayList<>();
@@ -68,29 +77,50 @@ public class StatsActivity extends AppCompatActivity {
 
     private void setupLineChar() {
         LineChart lineChart = findViewById(R.id.lineChart);
-        List<Entry> entries = new ArrayList<>();
-        List<Entry> entries1 = new ArrayList<>();
-        List<Entry> entries2 = new ArrayList<>();
+        List<Entry> diastolicEntry = new ArrayList<>();
+        List<Entry> systolicEntry = new ArrayList<>();
+        List<Entry> heartrateEntry = new ArrayList<>();
+        List<Reading> readings = readingManager.getReadings(this);
+        diastolicEntry.add(new Entry(0,0));
+        systolicEntry.add(new Entry(0,0));
+        heartrateEntry.add(new Entry(0,0));
 
-        for(int i =0;i<15;i++){
-            entries.add(new Entry(i*3,i*2));
-            entries1.add(new Entry(i*5,i*3));
-            entries2.add(new Entry(i*4,i/2));
+        for(int i =1;i<readings.size();i++){
+            Reading reading = readings.get(i);
+            Log.d("buggg",reading.dateTimeTaken.getDayOfMonth()+" "+ reading);
+            diastolicEntry.add(new Entry(i,reading.bpDiastolic));
+            systolicEntry.add(new Entry(i,reading.bpSystolic));
+            heartrateEntry.add(new Entry(i,reading.heartRateBPM));
+
 
         }
 
-        LineDataSet dataSet = new LineDataSet(entries,"HeartRate1");
-        LineDataSet dataSet1 = new LineDataSet(entries1,"HeartRate2");
-        LineDataSet dataSet2 = new LineDataSet(entries2,"HeartRate3");
+        LineDataSet diastolicDataSet = new LineDataSet(diastolicEntry,"BP Diastolic");
+        LineDataSet systolicDataSet = new LineDataSet(systolicEntry,"BP Systolic");
+        LineDataSet heartRateDataSet = new LineDataSet(heartrateEntry,"HearRate BPM");
 
-        dataSet.setColor(getResources().getColor(R.color.colorAccent));
-        dataSet1.setColor(getResources().getColor(R.color.purple));
-        dataSet2.setColor(getResources().getColor(R.color.orange));
-        dataSet.setCircleColor(getResources().getColor(R.color.colorAccent));
-        dataSet1.setCircleColor(getResources().getColor(R.color.purple));
-        dataSet2.setCircleColor(getResources().getColor(R.color.orange));
+        diastolicDataSet.setColor(getResources().getColor(R.color.colorAccent));
+        systolicDataSet.setColor(getResources().getColor(R.color.purple));
+        heartRateDataSet.setColor(getResources().getColor(R.color.orange));
 
-        dataSet.setCircleHoleRadius(0);
+        diastolicDataSet.setCircleColor(getResources().getColor(R.color.colorAccent));
+        systolicDataSet.setCircleColor(getResources().getColor(R.color.purple));
+        heartRateDataSet.setCircleColor(getResources().getColor(R.color.orange));
+
+        //this is to make the curve smooth
+        diastolicDataSet.setDrawCircleHole(false);
+        diastolicDataSet.setDrawCircles(false);
+        diastolicDataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+
+        systolicDataSet.setDrawCircleHole(false);
+        systolicDataSet.setDrawCircles(false);
+        systolicDataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+
+        heartRateDataSet.setDrawCircleHole(false);
+        heartRateDataSet.setDrawCircles(false);
+        heartRateDataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+
+        //remove unneccessy background lines
         lineChart.setDrawBorders(false);
         lineChart.setDrawGridBackground(false);
         lineChart.getAxisRight().setDrawLabels(false);
@@ -98,10 +128,14 @@ public class StatsActivity extends AppCompatActivity {
         lineChart.getXAxis().setDrawGridLines(false);
         lineChart.getAxisLeft().setDrawGridLines(false);
 
-        LineData lineData = new LineData(dataSet,dataSet1,dataSet2);
-
+        LineData lineData = new LineData(diastolicDataSet,systolicDataSet,heartRateDataSet);
+        lineData.setDrawValues(false);
+        lineData.setHighlightEnabled(false);
         lineChart.getXAxis().setDrawAxisLine(false);
         lineChart.setData(lineData);
+        //start at zero
+        lineChart.getAxisRight().setAxisMinimum(0);
+        lineChart.getAxisRight().setAxisMinimum(0);
         lineChart.invalidate();
     }
 }
