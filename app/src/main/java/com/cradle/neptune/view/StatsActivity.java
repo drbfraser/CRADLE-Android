b242b87d133aad4c881569b1b9f24a1c90238e17
sachin <sraturi@sfu.ca>
2019-11-18 13:13:31 -0800

    bar graph able to calculate all the different lights

--- app/src/main/java/com/cradle/neptune/view/StatsActivity.java
+++ app/src/main/java/com/cradle/neptune/view/StatsActivity.java
@@ -8,7 +8,9 @@ import android.util.Log;
 import com.cradle.neptune.R;
 import com.cradle.neptune.dagger.MyApp;
 import com.cradle.neptune.model.Reading;
+import com.cradle.neptune.model.ReadingAnalysis;
 import com.cradle.neptune.model.ReadingManager;
+import com.cradle.neptune.utilitiles.BarGraphValueFormatter;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.BarData;
@@ -17,6 +19,7 @@ import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.ValueFormatter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -41,21 +44,54 @@ public class StatsActivity extends AppCompatActivity {
 
         List<Reading> readings = readingManager.getReadings(this);
         BarChart barChart = findViewById(R.id.bargraph);
-        List<BarEntry> entries = new ArrayList<>();
-        List<BarEntry> entries1 = new ArrayList<>();
-        List<BarEntry> entries2 = new ArrayList<>();
+        List<BarEntry> greenEntry = new ArrayList<>();
+        List<BarEntry> yellowUpEntry = new ArrayList<>();
+        List<BarEntry> yellowDownEntry = new ArrayList<>();
+        List<BarEntry> redUpEntry = new ArrayList<>();
+        List<BarEntry> redDownEntry = new ArrayList<>();
+        int green =0;
+        int yellowup=0;
+        int yellowDown = 0;
+        int redDown =0;
+        int redUp =0;
 
-        entries.add(new BarEntry(3,22));
-        entries1.add(new BarEntry(1,18));
-        entries2.add(new BarEntry(2,8));
+        for(int i =0;i<readings.size();i++){
+            ReadingAnalysis analysis = ReadingAnalysis.analyze(readings.get(i));
+            if(analysis == ReadingAnalysis.RED_DOWN){
+                redDown++;
+            } else if(analysis==ReadingAnalysis.GREEN){
+                green++;
+            } else if (analysis==ReadingAnalysis.RED_UP){
+                redDown++;
+            } else if (analysis == ReadingAnalysis.YELLOW_UP){
+                yellowup++;
+            } else if (analysis==ReadingAnalysis.YELLOW_DOWN){
+                yellowDown++;
+            }
+        }
+        greenEntry.add(new BarEntry(1,green));
+        yellowUpEntry.add(new BarEntry(2,yellowup));
+        yellowDownEntry.add(new BarEntry(3,yellowDown));
+        redUpEntry.add(new BarEntry(4,redUp));
+        redDownEntry.add(new BarEntry(5,redDown));
 
-        BarDataSet dataSet = new BarDataSet(entries,"HeartRate1");
-        BarDataSet dataSet1 = new BarDataSet(entries1,"HeartRate2");
-        BarDataSet dataSet2 = new BarDataSet(entries2,"HeartRate3");
+        BarDataSet greenDataSet = new BarDataSet(greenEntry,"GREEN");
+        BarDataSet yellowUpDataSet = new BarDataSet(yellowUpEntry,"YELLOW UP");
+        BarDataSet yellowDownDataSet = new BarDataSet(yellowDownEntry,"YELLOW DOWN");
+        BarDataSet redUpDataSet = new BarDataSet(redUpEntry,"RED UP");
+        BarDataSet redDownDataSet = new BarDataSet(redDownEntry,"RED DOWN");
 
-        dataSet.setColor(Color.GREEN);
-        dataSet1.setColor(Color.RED);
-        dataSet2.setColor(Color.YELLOW);
+        greenDataSet.setValueFormatter(new BarGraphValueFormatter("Green"));
+        yellowDownDataSet.setValueFormatter(new BarGraphValueFormatter("Yellow Down"));
+        yellowUpDataSet.setValueFormatter(new BarGraphValueFormatter("Yellow Up"));
+        redDownDataSet.setValueFormatter(new BarGraphValueFormatter("Red Down"));
+        redUpDataSet.setValueFormatter(new BarGraphValueFormatter("Red Up"));
+
+        greenDataSet.setColor(Color.GREEN);
+        yellowUpDataSet.setColor(Color.YELLOW);
+        yellowDownDataSet.setColor(Color.YELLOW);
+        redDownDataSet.setColor(Color.RED);
+        redUpDataSet.setColor(Color.RED);
 
 
         barChart.setDrawBorders(false);
@@ -69,7 +105,7 @@ public class StatsActivity extends AppCompatActivity {
         barChart.getAxisRight().setAxisMinimum(0);
         barChart.getAxisLeft().setAxisMinimum(0);
 
-        BarData lineData = new BarData(dataSet,dataSet1,dataSet2);
+        BarData lineData = new BarData(greenDataSet,yellowUpDataSet,yellowDownDataSet,redUpDataSet,redDownDataSet);
 
         barChart.setData(lineData);
         barChart.invalidate();
