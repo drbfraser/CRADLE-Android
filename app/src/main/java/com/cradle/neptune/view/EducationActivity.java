4559317124beec36218abe7bcd896ca4980dcb28
sachin <sraturi@sfu.ca>
2020-02-25 17:15:33 -0800

    added new activity for education material

---
+++ app/src/main/java/com/cradle/neptune/view/EducationActivity.java
@@ -0,0 +1,16 @@
+package com.cradle.neptune.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cradle.neptune.R;
+
+public class EducationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_education);
+    }
+}
