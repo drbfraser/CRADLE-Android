ff47f76169b45dab86b9156b94edd663c021b65e
sachin <sraturi@sfu.ca>
2020-04-06 23:52:02 -0700

    setup recyclerview for HF

--- app/src/main/java/com/cradle/neptune/view/ui/settings/ui/settingnamedpairs/HealthFacilitiesActivity.java
+++ app/src/main/java/com/cradle/neptune/view/ui/settings/ui/settingnamedpairs/HealthFacilitiesActivity.java
@@ -1,12 +1,18 @@
 package com.cradle.neptune.view.ui.settings.ui.settingnamedpairs;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.cradle.neptune.R;
 import com.cradle.neptune.dagger.MyApp;
+import com.cradle.neptune.database.HealthFacilityEntity;
 import com.cradle.neptune.model.ReadingManager;
+import com.cradle.neptune.viewmodel.HealthFacilitiesAdapter;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -30,7 +36,14 @@ public class HealthFacilitiesActivity extends AppCompatActivity {
     }
 
     private void setupRecyclerview() {
+        RecyclerView recyclerView = findViewById(R.id.hfRecyclerView);
+        List<HealthFacilityEntity> healthFacilityEntities = readingManager.getAllFacilities();
+        HealthFacilitiesAdapter healthFacilitiesAdapter = new HealthFacilitiesAdapter(healthFacilityEntities);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
 
+        recyclerView.setAdapter(healthFacilitiesAdapter);
+        recyclerView.setLayoutManager(layoutManager);
+        healthFacilitiesAdapter.notifyDataSetChanged();
     }
 
     @Override
--- app/src/main/java/com/cradle/neptune/viewmodel/HealthFacilitiesAdapter.java
+++ app/src/main/java/com/cradle/neptune/viewmodel/HealthFacilitiesAdapter.java
@@ -1,17 +1,58 @@
 package com.cradle.neptune.viewmodel;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.cradle.neptune.R;
+import com.cradle.neptune.database.HealthFacilityEntity;
 
-public class HealthFacilitiesAdapter  {
+import java.util.List;
+
+
+public class HealthFacilitiesAdapter extends RecyclerView.Adapter<HealthFacilitiesAdapter.HealthFacilityViewHolder>  {
+
+    List<HealthFacilityEntity> healthFacilityEntities;
+
+    public HealthFacilitiesAdapter(List<HealthFacilityEntity> healthFacilityEntities) {
+        this.healthFacilityEntities = healthFacilityEntities;
+    }
+
+    @NonNull
+    @Override
+    public HealthFacilityViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.health_facility_layout, parent, false);
+        return new HealthFacilitiesAdapter.HealthFacilityViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HealthFacilityViewHolder holder, int position) {
+
+        HealthFacilityEntity healthFacilityEntity = healthFacilityEntities.get(position);
+
+        holder.locationTxt.setText(healthFacilityEntity.getLocation());
+        holder.nameTxt.setText(healthFacilityEntity.getName());
+        holder.phoneTxt.setText(healthFacilityEntity.getPhoneNumber());
+    }
+
+    @Override
+    public int getItemCount() {
+        return healthFacilityEntities.size();
+    }
 
     class HealthFacilityViewHolder extends RecyclerView.ViewHolder{
+        TextView phoneTxt, nameTxt, locationTxt;
 
         public HealthFacilityViewHolder(@NonNull View itemView) {
             super(itemView);
+            phoneTxt = itemView.findViewById(R.id.hfNumberTxt);
+            nameTxt = itemView.findViewById(R.id.hfNameTxt);
+            locationTxt = itemView.findViewById(R.id.hfLocationTxt);
         }
     }
 }
