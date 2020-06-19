0b9566e1b1779e42a96fcc0d595ab83d22872b28
Jeremy Schwartz <jdschwar@sfu.ca>
2020-06-19 15:49:48 -0700

    MOB-33 Additional Settings Fields

--- app/src/main/java/com/cradle/neptune/model/SettingsNew.kt
+++ app/src/main/java/com/cradle/neptune/model/SettingsNew.kt
@@ -10,21 +10,54 @@ import javax.inject.Inject
  * Holds app-wide settings which are persisted in Android's shared preference.
  */
 open class SettingsNew @Inject constructor(val sharedPreferences: SharedPreferences) {
+
+    /* Network */
+
     /**
-     * The server's hostname.
+     * The network hostname as configured in Settings > Advanced.
      */
     open val networkHostname: String?
         get() = sharedPreferences.getString("setting_server_hostname", null)
 
     /**
-     * The port to connect through.
+     * The network port as configured in Settings > Advanced.
      */
     open val networkPort: String?
         get() = sharedPreferences.getString("setting_server_port", null)
 
     /**
-     * Should we use HTTPS when connecting to the server?
+     * Whether to use HTTPS or not, configured in Settings > Advanced.
      */
     open val networkUseHttps: Boolean
         get() = sharedPreferences.getBoolean("setting_server_use_https", true)
+
+
+    /* VHT Info */
+
+    /**
+     * The user's name as configured in Settings.
+     */
+    open val vhtName: String?
+        get() = sharedPreferences.getString("setting_vht_name", null)
+
+    /**
+     * The user's region as configured in Settings.
+     */
+    open val region: String?
+        get() = sharedPreferences.getString("setting_region", null)
+
+
+    /* OCR */
+
+    /**
+     * Whether OCR is enabled or not, configured in Settings > Advanced.
+     */
+    open val isOcrEnabled: Boolean
+        get() = sharedPreferences.getBoolean("setting_ocr_enabled", true)
+
+    /**
+     * Whether OCR debug options are enabled or not, configured in Settings > Advanced.
+     */
+    open val isOcrDebugEnabled: Boolean
+        get() = sharedPreferences.getBoolean("setting_ocr_debug_enabled", false)
 }
