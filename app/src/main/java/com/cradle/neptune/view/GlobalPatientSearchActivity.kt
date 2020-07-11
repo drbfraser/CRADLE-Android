dde1ee4b81855b2c6eb62f37e74041b2080df42a
sraturi <sraturi@sfu.ca>
2020-07-11 01:11:55 -0700

    setup search query

--- app/src/main/java/com/cradle/neptune/view/GlobalPatientSearchActivity.kt
+++ app/src/main/java/com/cradle/neptune/view/GlobalPatientSearchActivity.kt
@@ -2,6 +2,8 @@ package com.cradle.neptune.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
 import com.cradle.neptune.R
 
 class GlobalPatientSearchActivity : AppCompatActivity() {
@@ -13,11 +15,31 @@ class GlobalPatientSearchActivity : AppCompatActivity() {
         if (supportActionBar != null) {
             supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         }
-
+        setupGlobalPatientSearch()
     }
+
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
+    }
+
+    private fun setupGlobalPatientSearch(){
+        val searchView:SearchView = findViewById(R.id.globalPatientSearchView)
+
+        searchView.setOnQueryTextListener(object :OnQueryTextListener {
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextSubmit(query: String): Boolean {
+                if (query=="")
+                    return false
+
+                return true
+            }
+
+        })
     }
 }
 
