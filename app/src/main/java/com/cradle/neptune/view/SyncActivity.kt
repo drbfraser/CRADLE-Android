71753829c00725ef4c1116bbf4ed0eec1da0ec4c
sraturi <sraturi@sfu.ca>
2020-07-27 19:37:49 -0700

    added sync activity

---
+++ app/src/main/java/com/cradle/neptune/view/SyncActivity.kt
@@ -0,0 +1,12 @@
+package com.cradle.neptune.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cradle.neptune.R
+
+class SyncActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sync)
+    }
+}
