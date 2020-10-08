1d933feead98482285bd6e2bdc7fbe6eef11f58b
Paul Ngo <paul_ngo@sfu.ca>
2020-10-07 20:47:43 -0700

    Verifiable: expand on comments and exceptions

--- app/src/main/java/com/cradle/neptune/model/Verifiable.kt
+++ app/src/main/java/com/cradle/neptune/model/Verifiable.kt
@@ -13,24 +13,24 @@ import kotlin.reflect.full.memberProperties
  */
 interface Verifiable<T : Any> {
     /**
-     * Determines if a value for a property is valid. What is valid is as defined by the class
+     * Determines if a value for a property is valid. What is valid is defined by the class
      * implementing this.
      *
-     * To implement this, you need to check the validity on a case-by-case basis, because each
-     * property has its own type, own conditions for validity, etc. It's likely that is implemented
-     * using a when statement to go through every property. It's suggested that true is returned for
-     * properties that don't need to be validated.
+     * To implement this, the class needs to check the validity of all of its properties on a
+     * case-by-case basis, because each property has its own type, own conditions for validity, etc.
+     * It's likely that this is implemented using a when statement to go through every property.
+     * It's suggested that true is returned for properties that don't need to be validated.
      *
      * @return whether [value] for the [property] of a given class is valid
      */
     fun isValueValid(property: KProperty<*>, value: Any?): Boolean
 }
 
-// Note: See https://discuss.kotlinlang.org/t/reified-generics-in-interface/1628/2 for idea of
-// using extension functions to incorporate reified types
 /**
  * Determines validity of value for [property]. Classes should override this and handle the case
  * if they have any public lateinit properties.
+ * Note: See https://discuss.kotlinlang.org/t/reified-generics-in-interface/1628/2 for the idea of
+ * using extension functions to incorporate reified types
  *
  * @throws UninitializedPropertyAccessException if checking a lateinit property
  * @return whether the value in the class's [property] is valid.
@@ -39,16 +39,20 @@ interface Verifiable<T : Any> {
 inline fun <reified T : Any> Verifiable<T>.isPropertyValid(property: KProperty<*>): Boolean {
     val memberProperty = T::class.memberProperties.find {
         property.name == it.name
-    } ?: throw IllegalArgumentException("property doesn't exist")
+    } ?: throw IllegalArgumentException(
+        "property ${property.name} doesn't exist for ${T::class.java.simpleName}"
+    )
 
     try {
         return isValueValid(memberProperty, memberProperty.get(this as T))
     } catch (e: InvocationTargetException) {
         if (e.cause is UninitializedPropertyAccessException) {
+            // Propagate any attempts to access uninitialized lateinit vars as this exception type.
             throw UninitializedPropertyAccessException(
                 (e.cause as UninitializedPropertyAccessException).message
             )
         }
+        // Otherwise, something else went wrong here and we should propagate that too.
         throw e
     }
 }
@@ -58,6 +62,11 @@ inline fun <reified T : Any> Verifiable<T>.isPropertyValid(property: KProperty<*
  * @return whether all the member values are valid
  */
 inline fun <reified T : Any> Verifiable<T>.areAllMemberValuesValid(): Boolean {
+    // We require KProperties of type KProperty1 in order to get the property value from the `this`
+    // object. To do this, we use reified type parameters which let us treat the type of the object
+    // as a class. We need the class access to use `memberProperties`, which hold KProperty1s.
+    // In order to use reified type parameters, we need to make this an inline function.
+    // See https://kotlinlang.org/docs/reference/inline-functions.html#reified-type-parameters.
     for (property in T::class.memberProperties) {
         if (!isValueValid(property, property.get(this as T))) {
             return false
