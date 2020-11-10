ea78cd1a9a2304dcfa998afe02f56548df8a0572
Paul Ngo <paul_ngo@sfu.ca>
2020-11-10 15:22:24 -0800

    pre-push hook: cleanup echos and run unit tests

--- hooks/pre-push.sh
+++ hooks/pre-push.sh
@@ -1,24 +1,40 @@
 #!/bin/sh
 
-echo "Running static analysis..."
+echo "hooks/pre-push.sh: Running static analysis (./gradlew detekt)..."
 
 # Validate Kotlin code with detekt
 ./gradlew app:detekt --no-daemon
+STATUS=$?
+if [ $STATUS -ne 0 ] ; then
+    cat <<EOF >&2
+**********************************************************
+hooks/pre-push.sh: Detekt failed
+Static code analysis found issues
 
-status=$?
-
-if [ "$status" = 0 ] ; then
-    echo "Static analysis found no issues. Ready to push."
-    exit 0
-else
-    echo 1>&2 "Static analysis found issues......"
-	  echo "**********************************************************"
-	  echo "                 Detekt failed                            "
-		echo ""
-		echo "   Use the command: ./gradlew detekt to run analysis"
-		echo ""
-		echo " Please fix and commit the above issues before pushing    "
-	  echo "**********************************************************"
+Use the command ./gradlew detekt or the detekt Gradle task
+in Android Studio to run the static code analysis for details.
+Please fix and commit the above issues before pushing
+**********************************************************
+EOF
     exit 1
 fi
 
+# Run unit tests before pushing
+echo "hooks/pre-push.sh: Running unit tests (./gradlew test)..."
+./gradlew test
+STATUS=$?
+if [ $STATUS -ne 0 ] ; then
+    cat <<EOF >&2
+**********************************************************
+hooks/pre-push.sh: Unit tests failed
+
+Use the command ./gradlew test or run the unit tests
+in Android Studio for details.
+Please fix and commit the above issues before pushing
+**********************************************************
+EOF
+    exit 1
+fi
+
+echo "hooks/pre-push.sh: Tests passed. Pushing to repo"
+exit 0
