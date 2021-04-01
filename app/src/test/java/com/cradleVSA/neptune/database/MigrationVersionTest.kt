e3dee587521cb37aee89571365c6d1cb44525454
Paul Ngo <paul_ngo@sfu.ca>
2021-03-31 21:31:04 -0700

    use Room's MigrationContainer to test migration paths

--- app/src/test/java/com/cradleVSA/neptune/database/MigrationVersionTest.kt
+++ app/src/test/java/com/cradleVSA/neptune/database/MigrationVersionTest.kt
@@ -1,51 +1,19 @@
 package com.cradleVSA.neptune.database
 
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withTimeout
+import androidx.room.RoomDatabase
 import org.junit.jupiter.api.Test
 
 class MigrationVersionTest {
     @Test
-    fun `test that there is a migration to current version`() {
-        assert(Migrations.ALL_MIGRATIONS.count { it.endVersion == CURRENT_DATABASE_VERSION } >= 1) {
-            "Missing a migration to database version $CURRENT_DATABASE_VERSION"
-        }
-    }
-
-    @Test
     fun `test that there is a migration path to the current version`() {
-        runBlocking {
-            withTimeout(10_000L) {
-                val startingVersion = 1
-                assert(isTherePathToTarget(startingVersion, CURRENT_DATABASE_VERSION)) {
-                    "Missing a migration to database version $CURRENT_DATABASE_VERSION"
-                }
+        val migrations = RoomDatabase.MigrationContainer().apply {
+            addMigrations(*Migrations.ALL_MIGRATIONS)
+        }
+        for (currentVersion in 1 until CURRENT_DATABASE_VERSION) {
+            val migrationPath = migrations.findMigrationPath(currentVersion, CURRENT_DATABASE_VERSION)
+            assert(!migrationPath.isNullOrEmpty()) {
+                "missing path from db version $currentVersion to db version $CURRENT_DATABASE_VERSION"
             }
         }
-    }
-
-    private fun isTherePathToTarget(currentVersion: Int, targetVersion: Int): Boolean {
-        if (currentVersion > targetVersion) {
-            // we don't support downgrades
-            return false
-        }
-        if (currentVersion == targetVersion)  {
-            return true
-        }
-
-        Migrations.ALL_MIGRATIONS
-            .filter { it.startVersion == currentVersion }
-            .also {
-                if (it.isEmpty()) {
-                    return false
-                }
-            }
-            .forEach {
-                if (isTherePathToTarget(it.endVersion, targetVersion)) {
-                    return true
-                }
-            }
-
-        return false
     }
 }
