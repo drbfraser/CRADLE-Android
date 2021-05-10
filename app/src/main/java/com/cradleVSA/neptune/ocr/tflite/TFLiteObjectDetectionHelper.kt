aa798a65c64101865bb12adac330d56168606720
Paul Ngo <paul_ngo@sfu.ca>
2021-05-09 18:55:18 -0700

    OCR: optimize image post-processing by using buffers
    Instead of needing to reallocate arrays all the time, we use
    TensorBuffers as class members and rewind the buffers for the next
    detections. As a result, the class is no longer thread-safe, but this
    doesn't matter, since we're doing sequential execution anyway for OCR.
    This should result in better perf; the TensorFlow Lite docs state that
    the Interpreter::runForMultipleInputsOutputs API is more efficient if
    buffers are used [1]. The optimization was adapted from the TensorFlow
    examples [2].
    
    [1] https://www.tensorflow.org/lite/api_docs/java/org/tensorflow/lite/Interpreter#public-void-runformultipleinputsoutputs-object[]-inputs,-mapinteger,-object-outputs
    [2] https://github.com/tensorflow/examples/blob/84d53a9bc37b95079d0cd9d247ba0e3d1ebbf538/lite/examples/image_classification/android/lib_support/src/main/java/org/tensorflow/lite/examples/classification/tflite/Classifier.java

--- app/src/main/java/com/cradleVSA/neptune/ocr/tflite/TFLiteObjectDetectionHelper.kt
+++ app/src/main/java/com/cradleVSA/neptune/ocr/tflite/TFLiteObjectDetectionHelper.kt
@@ -1,10 +1,10 @@
+@file:Suppress("MagicNumber")
 package com.cradleVSA.neptune.ocr.tflite
 
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.RectF
 import android.os.Build
-import android.util.Log
 import androidx.annotation.GuardedBy
 import androidx.collection.arrayMapOf
 import org.tensorflow.lite.Interpreter
@@ -13,146 +13,183 @@ import org.tensorflow.lite.support.common.FileUtil
 import org.tensorflow.lite.support.image.ImageProcessor
 import org.tensorflow.lite.support.image.TensorImage
 import org.tensorflow.lite.support.image.ops.ResizeWithCropOrPadOp
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.nio.ByteBuffer
 
 private const val TAG = "TFLiteHelper"
 private const val LABEL_FILE_NAME = "seven_seg_labelmap.txt"
 private const val MODEL_FILENAME = "seven_seg_ssd.tflite"
 
-// Only return this many results.
-private const val NUM_DETECTIONS = 6
-
 /**
- * Helper for using TFLite
+ * Helper for using TFLite. This class interprets the results from the [interpreter]
+ * into [Classifier.Recognition] objects.
  *
- * ref:
- * - github.com/tensorflow/models/tree/master/research/object_detection
+ * References:
+ * - https://github.com/tensorflow/models/tree/master/research/object_detection
  * - https://github.com/android/camera-samples/blob/main/CameraXTfLite
  * - https://github.com/tensorflow/examples/tree/master/lite/examples/image_classification/android
+ * - https://github.com/tensorflow/examples/blob/84d53a9bc37b95079d0cd9d247ba0e3d1ebbf538/lite/examples/image_classification/android/lib_support/src/main/java/org/tensorflow/lite/examples/classification/tflite/Classifier.java
+ *
+ * Note: This class is **not** thread-safe.
  */
 class TFLiteObjectDetectionHelper(context: Context) : Classifier {
     /**
      * Use Android Neural Network API if available. TensorFlow recommends using this for Android
      * Pie or above. https://www.tensorflow.org/lite/performance/nnapi
      */
-    private val nnApiDelegate: NnApiDelegate? =
+    @GuardedBy("this")
+    private var nnApiDelegate: NnApiDelegate? =
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             NnApiDelegate()
         } else {
             null
         }
 
-    private val interpreterLock = Object()
+    @GuardedBy("this")
+    private var interpreter: Interpreter?
+    private val inputImageBuffer: TensorImage
+    private val inputImageProcessor: ImageProcessor
+    private val inputImageSizeX: Int
+    private val inputImageSizeY: Int
 
-    @GuardedBy("interpreterLock")
-    private var interpreter: Interpreter? =
-        Interpreter(
+    private val numDetections: Int
+
+    /**
+     * Array of shape [batchsize, numDetections, 4].
+     * Contains the location of detected boxes. The locations are an array of floats in the
+     * closed interval [0, 1] corresponding to {top, left, bottom, right}.
+     *
+     * Note: The implementation of this array is flattened: Instead of being a
+     * float[numDetections][4] (numDetections by 4) matrix, it's flattened so that every group of 4
+     * entries corresponds to a detection. The [getTop], [getLeft], [getBottom], [getRight] methods
+     * handles this abstraction.
+     */
+    @JvmInline
+    private value class LocationTensorBuffer(val tensorBuffer: TensorBuffer) {
+        /** Returns the data buffer of the underlying [TensorBuffer] */
+        val buffer: ByteBuffer get() = tensorBuffer.buffer
+
+        fun getTop(detectionIndex: Int) = tensorBuffer.getFloatValue(4 * detectionIndex + 0)
+        fun getLeft(detectionIndex: Int) = tensorBuffer.getFloatValue(4 * detectionIndex + 1)
+        fun getBottom(detectionIndex: Int) = tensorBuffer.getFloatValue(4 * detectionIndex + 2)
+        fun getRight(detectionIndex: Int) = tensorBuffer.getFloatValue(4 * detectionIndex + 3)
+    }
+    private val outputLocationBuffer: LocationTensorBuffer
+
+    /**
+     * Array of shape [batchsize, numDetections]
+     * Contains the classes of detected boxes
+     */
+    private val outputClassesBuffer: TensorBuffer
+
+    /**
+     * Array of shape [batchsize, numDetections].
+     * Contains the scores / confidence of detected boxes.
+     */
+    private val outputScoresBuffer: TensorBuffer
+
+    /**
+     * Array of shape [ batchsize ]. This contains the number of detected boxes
+     */
+    private val outputNumDetectionsBuffer: TensorBuffer
+    init {
+        val interpreter = Interpreter(
             FileUtil.loadMappedFile(context, MODEL_FILENAME),
             Interpreter.Options().apply {
                 nnApiDelegate?.let { addDelegate(it) }
             }
         )
+        this.interpreter = interpreter
 
-    private val inputImageBuffer: TensorImage
-    private val inputImageProcessor: ImageProcessor
-    private val inputImageSizeX: Int
-    private val inputImageSizeY: Int
-    init {
-        interpreter!!.let { interpreter ->
-            val inputTensor = interpreter.getInputTensor(0)
-            inputTensor.shape().let { shape ->
-                // {1, height, width, 3}
-                inputImageSizeX = shape[2]
-                inputImageSizeY = shape[1]
-            }
+        val inputTensor = interpreter.getInputTensor(0)
+        inputTensor.shape().let { shape ->
+            // {1, height, width, 3}
+            inputImageSizeX = shape[2]
+            inputImageSizeY = shape[1]
+        }
 
-            inputImageBuffer = TensorImage(inputTensor.dataType())
-            inputImageProcessor = ImageProcessor.Builder()
-                .add(ResizeWithCropOrPadOp(inputImageSizeX, inputImageSizeY))
-                .build()
+        inputImageBuffer = TensorImage(inputTensor.dataType())
+        inputImageProcessor = ImageProcessor.Builder()
+            .add(ResizeWithCropOrPadOp(inputImageSizeX, inputImageSizeY))
+            .build()
 
-            val outputTensors = (0 until interpreter.outputTensorCount).map { interpreter.getOutputTensor(it) }
-            val outputTensorInfo = outputTensors.map { outputTensor ->
-                """name: ${outputTensor.name()}, shape: ${outputTensor.shape().toList()}, dataType: ${outputTensor.dataType()}"""
-            }
+        require(interpreter.outputTensorCount == 4) { "unexpected TFLite model" }
 
-            Log.d(TAG, """
-                DEBUG: Tensor information:
-                Output tensors:
-                $outputTensorInfo
-            """.trimIndent())
+        interpreter.getOutputTensor(0).let {
+            outputLocationBuffer = LocationTensorBuffer(
+                TensorBuffer.createFixedSize(it.shape(), it.dataType())
+            )
+        }
+        interpreter.getOutputTensor(1).let {
+            outputClassesBuffer = TensorBuffer.createFixedSize(it.shape(), it.dataType())
+            numDetections = it.shape()[1]
+        }
+        interpreter.getOutputTensor(2).let {
+            outputScoresBuffer = TensorBuffer.createFixedSize(it.shape(), it.dataType())
+        }
+        interpreter.getOutputTensor(3).let {
+            outputNumDetectionsBuffer = TensorBuffer.createFixedSize(it.shape(), it.dataType())
+        }
+
+        require(outputLocationBuffer.tensorBuffer.flatSize == numDetections * 4) {
+            "location buffer flatsize needs to be numDetections * 4 since each detection " +
+                "should have 4 rectangle coordinates each"
         }
     }
 
     private val labels: List<String> = FileUtil.loadLabels(context, LABEL_FILE_NAME)
 
-    @Suppress("MagicNumber")
     override fun recognizeItemsInImage(bitmap: Bitmap): List<Classifier.Recognition> {
         val tfImageBuffer: TensorImage = inputImageBuffer.load(bitmap)
             .run { inputImageProcessor.process(inputImageBuffer) }
 
         // Copy the input data into TensorFlow.
-        // Array of shape [Batchsize, NUM_DETECTIONS,4]. Contains the location of detected boxes
-        val outputLocations = arrayOf(Array(NUM_DETECTIONS) { FloatArray(4) })
-        // Array of shape [Batchsize, NUM_DETECTIONS]
-        // contains the classes of detected boxes
-        val outputClasses = arrayOf(FloatArray(NUM_DETECTIONS))
-        // Array of shape [Batchsize, NUM_DETECTIONS]. Contains the scores / confidence of detected
-        // boxes
-        val outputScores = arrayOf(FloatArray(NUM_DETECTIONS))
-        // numDetections: array of shape [Batchsize]. This contains the number of detected boxes
-        val numDetections = FloatArray(1)
-
         val inputArray = arrayOf<Any>(tfImageBuffer.buffer)
         val outputMap: Map<Int, Any> = arrayMapOf(
-            0 to outputLocations,
-            1 to outputClasses,
-            2 to outputScores,
-            3 to numDetections
+            0 to outputLocationBuffer.buffer.rewind(),
+            1 to outputClassesBuffer.buffer.rewind(),
+            2 to outputScoresBuffer.buffer.rewind(),
+            3 to outputNumDetectionsBuffer.buffer.rewind()
         )
 
         // Run TFLite, but don't do anything if it's closed
-        synchronized(interpreterLock) {
+        synchronized(this) {
             interpreter?.runForMultipleInputsOutputs(inputArray, outputMap) ?: return emptyList()
         }
 
-        // Show the best detections after scaling them back to the input size.
-        return (0 until NUM_DETECTIONS).map { i ->
+        // Show the detections after scaling them back to the input size.
+        return (0 until numDetections).map { i ->
             // SSD Mobilenet V1 Model assumes class 0 is background class
             // in label file and class labels start from 1 to number_of_classes+1,
             // while outputClasses correspond to class index from 0 to number_of_classes
             val labelOffset = 1
             val title = try {
-                labels[outputClasses[0][i].toInt() + labelOffset]
+                labels[outputClassesBuffer.getFloatValue(i).toInt() + labelOffset]
             } catch (e: IndexOutOfBoundsException) {
                 ""
             }
 
-            val scores = outputScores[0][i]
+            val scores = outputScoresBuffer.getFloatValue(i)
 
-            val detection = outputLocations[0][i].let { locations ->
-                // The locations are an array of [0, 1] floats for
-                // [top, left, bottom, right]
-                val top = locations[0]
-                val left = locations[1]
-                val bottom = locations[2]
-                val right = locations[3]
+            val location = outputLocationBuffer.let {
                 RectF(
-                    left * inputImageSizeX,
-                    top * inputImageSizeY,
-                    right * inputImageSizeX,
-                    bottom * inputImageSizeY
+                    it.getLeft(detectionIndex = i) * inputImageSizeX,
+                    it.getTop(detectionIndex = i) * inputImageSizeY,
+                    it.getRight(detectionIndex = i) * inputImageSizeX,
+                    it.getBottom(detectionIndex = i) * inputImageSizeY
                 )
             }
 
-            Classifier.Recognition("$i", title, scores, detection)
+            Classifier.Recognition("$i", title, scores, location)
         }
     }
 
     override fun close() {
-        synchronized(interpreterLock) {
+        synchronized(this) {
             interpreter?.close()
             interpreter = null
             nnApiDelegate?.close()
+            nnApiDelegate = null
         }
     }
 }
