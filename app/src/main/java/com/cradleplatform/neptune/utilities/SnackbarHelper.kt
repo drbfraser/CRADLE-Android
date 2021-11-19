7f251a0478cdf4ebfae6b8e1ddef1f22b5170643
Henry Fang <fanghaof@sfu.ca>
2021-11-19 01:05:42 -0800

    Update SnackbarHelper class

--- app/src/main/java/com/cradleplatform/neptune/utilities/SnackbarHelper.kt
+++ app/src/main/java/com/cradleplatform/neptune/utilities/SnackbarHelper.kt
@@ -1,22 +1,38 @@
 package com.cradleplatform.neptune.utilities
 
 import android.app.Activity
-import android.content.Context
+import android.os.Build
 import android.view.View
+import androidx.annotation.RequiresApi
+import com.cradleplatform.neptune.R
 import com.google.android.material.snackbar.BaseTransientBottomBar
 import com.google.android.material.snackbar.Snackbar
 
 class SnackbarHelper {
     companion object {
-        fun showSnackbar(activity: Activity, message: String) {
+        @RequiresApi(Build.VERSION_CODES.M)
+        fun showSnackbarWithOK(activity: Activity, message: String) {
             Snackbar
                 .make(
                     activity.findViewById(android.R.id.content),
                     message,
                     Snackbar.LENGTH_INDEFINITE)
-                .setAction(activity.getString(android.R.string.ok), null)
+                .setAction(activity.getString(android.R.string.ok), View.OnClickListener {  })
                 .setAnimationMode(BaseTransientBottomBar.ANIMATION_MODE_FADE)
-                // .setActionTextColor(android.graphics.Color.W)
+                .setActionTextColor(activity.getColor(R.color.white))
+                .show()
+        }
+
+        @RequiresApi(Build.VERSION_CODES.M)
+        fun showSnackbarWithError(activity: Activity, message: String) {
+            Snackbar
+                .make(
+                    activity.findViewById(android.R.id.content),
+                    message,
+                    Snackbar.LENGTH_INDEFINITE)
+                .setAction(activity.getString(android.R.string.ok), View.OnClickListener {  })
+                .setAnimationMode(BaseTransientBottomBar.ANIMATION_MODE_FADE)
+                .setActionTextColor(activity.getColor(R.color.redDown))
                 .show()
         }
     }
