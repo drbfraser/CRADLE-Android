90aac4c8c51b8e0153bd6b5133515d5557cbdc73
zou yang <18979193009@163.com>
2022-06-14 21:15:26 -0700

    - Reformat the code

--- app/src/main/java/com/cradleplatform/neptune/model/FormTemplate.kt
+++ app/src/main/java/com/cradleplatform/neptune/model/FormTemplate.kt
@@ -9,49 +9,49 @@ Can be updated for newer version of templates.
 Currently using Gson, could use another library if needed.
 */
 
-data class FormTemplate (
+data class FormTemplate(
 
-	@SerializedName("version") val version : String,
-	@SerializedName("name") val name : String,
-	@SerializedName("dateCreated") val dateCreated : Int,
-	@SerializedName("category") val category : String,
-	@SerializedName("id") val id : String,
-	@SerializedName("lastEdited") val lastEdited : Int,
-	@SerializedName("lang") val lang : String,
-	@SerializedName("questions") val questions : List<Questions>
+    @SerializedName("version") val version: String,
+    @SerializedName("name") val name: String,
+    @SerializedName("dateCreated") val dateCreated: Int,
+    @SerializedName("category") val category: String,
+    @SerializedName("id") val id: String,
+    @SerializedName("lastEdited") val lastEdited: Int,
+    @SerializedName("lang") val lang: String,
+    @SerializedName("questions") val questions: List<Questions>
 )
 
-data class Questions (
+data class Questions(
 
-	@SerializedName("id") val id : String,
-	@SerializedName("visibleCondition") val visibleCondition : List<VisibleCondition>,
-	@SerializedName("isBlank") val isBlank : Boolean,
-	@SerializedName("formTemplateId") val formTemplateId : String,
-	@SerializedName("mcOptions") val mcOptions : List<McOptions>,
-	@SerializedName("questionIndex") val questionIndex : Int,
-	@SerializedName("numMin") val numMin : Double,
-	@SerializedName("questionId") val questionId : String,
-	@SerializedName("questionText") val questionText : String,
-	@SerializedName("questionType") val questionType : String,
-	@SerializedName("answers") val answers : Answers,
-	@SerializedName("hasCommentAttached") val hasCommentAttached : Boolean,
-	@SerializedName("required") val required : Boolean
+    @SerializedName("id") val id: String,
+    @SerializedName("visibleCondition") val visibleCondition: List<VisibleCondition>,
+    @SerializedName("isBlank") val isBlank: Boolean,
+    @SerializedName("formTemplateId") val formTemplateId: String,
+    @SerializedName("mcOptions") val mcOptions: List<McOptions>,
+    @SerializedName("questionIndex") val questionIndex: Int,
+    @SerializedName("numMin") val numMin: Double,
+    @SerializedName("questionId") val questionId: String,
+    @SerializedName("questionText") val questionText: String,
+    @SerializedName("questionType") val questionType: String,
+    @SerializedName("answers") val answers: Answers,
+    @SerializedName("hasCommentAttached") val hasCommentAttached: Boolean,
+    @SerializedName("required") val required: Boolean
 )
 
-data class McOptions (
+data class McOptions(
 
-	@SerializedName("mcid") val mcid : Int,
-	@SerializedName("opt") val opt : String
+    @SerializedName("mcid") val mcid: Int,
+    @SerializedName("opt") val opt: String
 )
 
-data class VisibleCondition (
+data class VisibleCondition(
 
-	@SerializedName("qidx") val qidx : Int,
-	@SerializedName("relation") val relation : String,
-	@SerializedName("answers") val answers : Answers
+    @SerializedName("qidx") val qidx: Int,
+    @SerializedName("relation") val relation: String,
+    @SerializedName("answers") val answers: Answers
 )
 
-data class Answers (
+data class Answers(
 
-	@SerializedName("answers") val answers : String?,
+    @SerializedName("answers") val answers: String?,
 )
