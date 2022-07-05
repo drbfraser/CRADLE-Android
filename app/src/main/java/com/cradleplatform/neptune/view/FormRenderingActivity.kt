6e6851de6a471c378f4ecc4915b21a68f93664c6
zou yang <18979193009@163.com>
2022-07-04 23:02:55 -0700

    Separate the form by section

--- app/src/main/java/com/cradleplatform/neptune/view/FormRenderingActivity.kt
+++ app/src/main/java/com/cradleplatform/neptune/view/FormRenderingActivity.kt
@@ -3,11 +3,15 @@ package com.cradleplatform.neptune.view
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.cradleplatform.neptune.R
 import com.cradleplatform.neptune.model.FormTemplate
+import com.cradleplatform.neptune.model.Questions
 import com.cradleplatform.neptune.model.RecyclerAdapter
 
 class FormRenderingActivity : AppCompatActivity() {
@@ -15,18 +19,45 @@ class FormRenderingActivity : AppCompatActivity() {
     private var layoutManager: RecyclerView.LayoutManager? = null
     private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
     private var form: FormTemplate? = null
+    private var btnNext: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_rendering)
 
         form = intent.getSerializableExtra(EXTRA_FORM_TEMPLATE) as FormTemplate
+        var id = intent.getStringExtra(EXTRA_PATIENT_ID)
+
+        //Check if question list contains category
+        if (getNumOfCategory(form!!) <= 0) {
+            val intent = Intent(this, FormSelectionActivity::class.java)
+            intent.putExtra(EXTRA_PATIENT_ID, id)
+            startActivity(intent)
+            finish()
+        }
+
+        var i = getNumOfCategory(form!!)
+        Toast.makeText(this, "$i pages remaining", Toast.LENGTH_SHORT).show()
+
         layoutManager = LinearLayoutManager(this)
 
         var recyclerView = findViewById<RecyclerView>(R.id.myRecyclerView)
         recyclerView.layoutManager = layoutManager
 
-        adapter = RecyclerAdapter(form!!)
+        btnNext = findViewById(R.id.btn_submit)
+        if (getNumOfCategory(getRestCategory(form!!)) == 0) {
+            btnNext?.text = "Submit"
+        }
+
+        btnNext?.setOnClickListener {
+            val newForm: FormTemplate = getRestCategory(form!!)
+            val intent = makeIntentWithFormTemplate(this, newForm, id!!)
+            startActivity(intent)
+        }
+
+        val firstCategory: FormTemplate = getFirstCategory(form!!)
+
+        adapter = RecyclerAdapter(firstCategory)
         recyclerView.adapter = adapter
     }
 
@@ -47,5 +78,59 @@ class FormRenderingActivity : AppCompatActivity() {
                 this.putExtras(bundle)
             }
         }
+    }
+
+    fun getNumOfCategory(form: FormTemplate): Int {
+        var num = 0
+        if (form.questions.isEmpty()) {
+            return num
+        }
+        for (question in form.questions) {
+            if (question.questionType == "CATEGORY") {
+                num += 1
+            }
+        }
+        return num
+    }
+
+    fun getFirstCategory(form: FormTemplate): FormTemplate {
+        Log.d("DEBUG: form before", form.questions.toString())
+        var questionList: List<Questions> = form.questions
+        var firstQuestionList: MutableList<Questions> = mutableListOf()
+        for (i in questionList.indices) {
+            if (questionList[i].questionType == "CATEGORY" && i != 0) {
+                break
+            } else {
+                firstQuestionList.add(questionList[i])
+            }
+        }
+
+        var newForm: FormTemplate = FormTemplate(
+            form.version, form.name, form.dateCreated,
+            form.category, form.id, form.lastEdited, form.lang, firstQuestionList.toList()
+        )
+
+        return newForm
+    }
+
+    fun getRestCategory(form: FormTemplate): FormTemplate {
+        var questionList: List<Questions> = form.questions
+        var restQuestionList: MutableList<Questions> = mutableListOf()
+        var notFirstCATEGORY: Boolean = false
+        for (i in questionList.indices) {
+            if (questionList[i].questionType == "CATEGORY" && i != 0) {
+                notFirstCATEGORY = true
+            }
+            if (notFirstCATEGORY) {
+                restQuestionList.add(questionList[i])
+            }
+        }
+
+        var newForm: FormTemplate = FormTemplate(
+            form.version, form.name, form.dateCreated,
+            form.category, form.id, form.lastEdited, form.lang, restQuestionList.toList()
+        )
+
+        return newForm
     }
 }
