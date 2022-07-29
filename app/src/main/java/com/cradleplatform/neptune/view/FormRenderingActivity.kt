1ea23579b2de416c708c997a7253fdbd65db8ca9
kyl42 <kyl42@sfu.ca>
2022-07-29 08:31:52 +0800

    Update FormRenderingActivity to use new FormTemplate layout

--- app/src/main/java/com/cradleplatform/neptune/view/FormRenderingActivity.kt
+++ app/src/main/java/com/cradleplatform/neptune/view/FormRenderingActivity.kt
@@ -37,6 +37,9 @@ class FormRenderingActivity : AppCompatActivity() {
         var i = getNumOfCategory(form!!)
         Toast.makeText(this, "$i pages remaining", Toast.LENGTH_SHORT).show()
 
+        val languageSelected = intent.getStringExtra(EXTRA_LANGUAGE_SELECTED)
+            ?: error("language selection missing from FormRenderingActivity Intent")
+
         layoutManager = LinearLayoutManager(this)
 
         var recyclerView = findViewById<RecyclerView>(R.id.myRecyclerView)
@@ -49,30 +52,38 @@ class FormRenderingActivity : AppCompatActivity() {
 
         btnNext?.setOnClickListener {
             val newForm: FormTemplate = getRestCategory(form!!)
-            val intent = makeIntentWithFormTemplate(this, newForm, id!!)
+            val intent = makeIntentWithFormTemplate(
+                this,
+                newForm,
+                id!!,
+                languageSelected
+            )
             startActivity(intent)
         }
 
         val firstCategory: FormTemplate = getFirstCategory(form!!)
 
-        adapter = RecyclerAdapter(firstCategory)
+        adapter = RecyclerAdapter(firstCategory, languageSelected)
         recyclerView.adapter = adapter
     }
 
     companion object {
         private const val EXTRA_FORM_TEMPLATE = "JSON string for form template"
         private const val EXTRA_PATIENT_ID = "Patient id that the form is created for"
+        private const val EXTRA_LANGUAGE_SELECTED = "String of language selected for a FormTemplate"
 
         @JvmStatic
         fun makeIntentWithFormTemplate(
             context: Context,
             formTemplate: FormTemplate,
+            formLanguage: String,
             patientId: String
         ): Intent {
             val bundle = Bundle()
             bundle.putSerializable(EXTRA_FORM_TEMPLATE, formTemplate)
             return Intent(context, FormRenderingActivity::class.java).apply {
                 this.putExtra(EXTRA_PATIENT_ID, patientId)
+                this.putExtra(EXTRA_LANGUAGE_SELECTED, formLanguage)
                 this.putExtras(bundle)
             }
         }
@@ -102,12 +113,7 @@ class FormRenderingActivity : AppCompatActivity() {
             }
         }
 
-        var newForm: FormTemplate = FormTemplate(
-            form.version, form.name, form.dateCreated,
-            form.id, form.lastEdited, form.lang, firstQuestionList.toList()
-        )
-
-        return newForm
+        return form.copy(questions = firstQuestionList.toList())
     }
 
     fun getRestCategory(form: FormTemplate): FormTemplate {
@@ -123,11 +129,6 @@ class FormRenderingActivity : AppCompatActivity() {
             }
         }
 
-        var newForm: FormTemplate = FormTemplate(
-            form.version, form.name, form.dateCreated,
-            form.id, form.lastEdited, form.lang, restQuestionList.toList()
-        )
-
-        return newForm
+        return form.copy(questions = restQuestionList.toList())
     }
 }
