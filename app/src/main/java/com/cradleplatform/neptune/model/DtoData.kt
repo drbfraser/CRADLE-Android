9faae7afd91f362b75b73252e4cdcbf5f0a747f2
zou yang <18979193009@163.com>
2022-07-31 15:48:54 -0700

    Created Data Transfer Object class for form submission

---
+++ app/src/main/java/com/cradleplatform/neptune/model/DtoData.kt
@@ -0,0 +1,11 @@
+package com.cradleplatform.neptune.model
+
+class DtoData {
+
+    companion object {
+        var form = mutableListOf<Pair<Int, String>>()
+        var resultForm: FormTemplate? = null
+        var template: FormTemplate? = null
+    }
+
+}
