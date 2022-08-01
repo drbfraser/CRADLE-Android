09467b4a09348963cbe4cda1f272eefa4718c763
zou yang <18979193009@163.com>
2022-07-31 19:11:15 -0700

    Updated MyHttpClient

--- app/src/main/java/com/cradleplatform/neptune/net/MyHttpClient.kt
+++ app/src/main/java/com/cradleplatform/neptune/net/MyHttpClient.kt
@@ -1,19 +1,20 @@
-package com.cradleplatform.neptune.net;
+package com.cradleplatform.neptune.net
 
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
-import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import com.loopj.android.http.RequestParams
 
-public class MyHttpClient {
-
-    private static final AsyncHttpClient client = new AsyncHttpClient();
-
-    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
-        client.get(url, params, responseHandler);
+object MyHttpClient {
+    private val client = AsyncHttpClient()
+    operator fun get(
+        url: String?,
+        params: RequestParams?,
+        responseHandler: AsyncHttpResponseHandler?
+    ) {
+        client[url, params, responseHandler]
     }
 
-    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
-        client.post(url, params, responseHandler);
+    fun post(url: String?, params: RequestParams?, responseHandler: AsyncHttpResponseHandler?) {
+        client.post(url, params, responseHandler)
     }
-
 }
