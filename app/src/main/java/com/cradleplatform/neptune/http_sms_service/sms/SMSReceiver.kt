e694d707e4bfdddba5ba7f559093c57882f49d5a
khishh <khiraishi0319@gmail.com>
2023-04-02 10:31:01 -0700

    make detekt happy

--- app/src/main/java/com/cradleplatform/neptune/http_sms_service/sms/SMSReceiver.kt
+++ app/src/main/java/com/cradleplatform/neptune/http_sms_service/sms/SMSReceiver.kt
@@ -6,7 +6,7 @@ import android.content.Intent
 import android.telephony.SmsMessage
 
 // Might want to encode the acknowledgment in the future
-private val ACK_REGEX_PATTERN = Regex("^01-CRADLE-\\d{6}-\\d{3}-ACK$")
+private val ackRegexPattern = Regex("^01-CRADLE-\\d{6}-\\d{3}-ACK$")
 
 class SMSReceiver(private val smsSender: SMSSender, private val relayPhoneNumber: String) : BroadcastReceiver() {
 
@@ -19,7 +19,7 @@ class SMSReceiver(private val smsSender: SMSSender, private val relayPhoneNumber
             val smsMessage = SmsMessage.createFromPdu(element as ByteArray?) ?: continue
             val messageBody = smsMessage.messageBody
             if (smsMessage.originatingAddress.equals(relayPhoneNumber) && messageBody.matches(
-                    ACK_REGEX_PATTERN
+                    ackRegexPattern
                 )
             ) {
                 smsSender.sendSmsMessage(true)
