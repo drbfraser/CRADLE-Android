caf35164d02c83157472a2efa2121a3a25ced257
Amirhossein Etaati <aea38@sfu.ca>
2023-07-17 11:12:10 -0700

    Remove unused import

--- app/src/main/java/com/cradleplatform/neptune/view/DashBoardActivity.kt
+++ app/src/main/java/com/cradleplatform/neptune/view/DashBoardActivity.kt
@@ -7,7 +7,6 @@ import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.telephony.TelephonyManager
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
