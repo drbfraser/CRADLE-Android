a6d3f2a2f513fa4a370f408d150eb6882ae0312f
InderdeepM <inderdeepmanan@gmail.com>
2023-08-02 13:31:44 -0700

    Add dialog when new number is found

--- app/src/main/java/com/cradleplatform/neptune/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/cradleplatform/neptune/viewmodel/UserViewModel.kt
@@ -6,15 +6,12 @@ import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.telephony.TelephonyManager
 import androidx.core.app.ActivityCompat
-import androidx.core.content.edit
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import com.cradleplatform.neptune.http_sms_service.http.RestApi
 import com.cradleplatform.neptune.manager.LoginManager
 import com.cradleplatform.neptune.manager.LoginManager.Companion.CURRENT_PHONE_NUMBER
 import com.cradleplatform.neptune.manager.LoginManager.Companion.PHONE_NUMBERS
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -28,7 +25,7 @@ class UserViewModel @Inject constructor(
 
     private lateinit var currentPhoneNumber: String
 
-    fun updateUserPhoneNumbers() {
+    fun isNewNumber(): String {
         if (ActivityCompat.checkSelfPermission(getApplication(), Manifest.permission.READ_SMS) ==
             PackageManager.PERMISSION_GRANTED
             && ActivityCompat.checkSelfPermission(getApplication(), Manifest.permission.READ_PHONE_NUMBERS) ==
@@ -44,36 +41,34 @@ class UserViewModel @Inject constructor(
                 // Get the list of the user phone numbers that were fetched at login
                 val allPhoneNumbers = parsePhoneNumberString(sharedPreferences.getString(PHONE_NUMBERS, ""))
                 // Check if fetchedPhoneNumber is in allPhoneNumbers
-                if(!allPhoneNumbers.contains(fetchedPhoneNumber)){
-                    // TODO: Let user know that a new phone number has been detected
-                    // TODO: Ask user if they want to update their phone number
-
-                    // Add the user's phone number to the database. TODO: if the user selects yet
-                    //
-                    viewModelScope.launch {
-                        val userId = sharedPreferences.getInt(LoginManager.USER_ID_KEY, -1)
-                        if (userId != -1) {
-                            val result = restApi.postUserPhoneNumber(userId, fetchedPhoneNumber)
-                            println("Debug-number-view-model: updating the database -> result = $result")
-                        }
-                    }
-                    //TODO:
-                    // Set the current phone number - This number is the source of SMS and will be used to validate user
-                    // sharedPreferences.edit().putString(CURRENT_PHONE_NUMBER, fetchedPhoneNumber).apply()
-                    println("Debug-number-view-model: A new phone number has been detected")
-                    println("Debug-number-view-model: fetchedPhoneNumber = $fetchedPhoneNumber")
-                    println("Debug-number-view-model: allPhoneNumbers = $allPhoneNumbers")
+                if (!allPhoneNumbers.contains(fetchedPhoneNumber)){
+                    return fetchedPhoneNumber
                 }
-                else {
-                //    TODO: Remove - for debugging only
-                    println("Debug-number-view-model: The phone number is already in the database")
-                    println("Debug-number-view-model: fetchedPhoneNumber = $fetchedPhoneNumber")
-                    println("Debug-number-view-model: allPhoneNumbers = $allPhoneNumbers")
-                }
-
             }
-        } // else: user doesn't exist or permission is not granted
+        }
+        // Is not a new number
+        return ""
     }
+
+    fun updateUserPhoneNumbers(phoneNumber: String) {
+        // Add the user's phone number to the database.
+        viewModelScope.launch {
+            val userId = sharedPreferences.getInt(LoginManager.USER_ID_KEY, -1)
+            if (userId != -1) {
+                val result = restApi.postUserPhoneNumber(userId, phoneNumber)
+                println("Debug-number-view-model: updating the database -> result = $result")
+            }
+        }
+
+        val allPhoneNumbers = parsePhoneNumberString(sharedPreferences.getString(PHONE_NUMBERS, ""))
+        //TODO:
+        // Set the current phone number - This number is the source of SMS and will be used to validate user
+        // sharedPreferences.edit().putString(CURRENT_PHONE_NUMBER, fetchedPhoneNumber).apply()
+        println("Debug-number-view-model: A new phone number has been detected")
+        println("Debug-number-view-model: fetchedPhoneNumber = $phoneNumber")
+        println("Debug-number-view-model: allPhoneNumbers = $allPhoneNumbers")
+    }
+
 
     fun getCurrentUserPhoneNumber() : String {
         return currentPhoneNumber
