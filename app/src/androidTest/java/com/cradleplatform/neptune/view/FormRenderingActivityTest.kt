7807260b4a556c941aaaf7b5d3116fffa898bbe7
Ekam Hothi <ehothi@sfu.ca>
2023-10-23 15:34:53 -0700

    Finished the categories test and added comments to explain the test

--- app/src/androidTest/java/com/cradleplatform/neptune/view/FormRenderingActivityTest.kt
+++ app/src/androidTest/java/com/cradleplatform/neptune/view/FormRenderingActivityTest.kt
@@ -34,6 +34,7 @@ class FormRenderingActivityTest{
     @get:Rule
     var activityScenarioRule = activityScenarioRule<DashBoardActivity>()
 
+    // Takes a list of questions and starts the forms activity with those questions.
     private fun startActivityWithQuestions(questions: List<Question>){
         activityScenarioRule.scenario.onActivity { activity ->
             val intent = FormRenderingActivity.makeIntentWithFormTemplate(
@@ -50,10 +51,13 @@ class FormRenderingActivityTest{
     @Test
     fun test_Categories(){
         val questions = mutableListOf<Question>()
+        val visibleCondition = listOf(VisibleCondition(null, null, null));
+        // Create 10 categories and questions to test
+        // Note that the 10th category has no questions so it must not appear
         for (i in 1..10) {
             val stringQuestion = Question(
                 id = "Q$i",
-                visibleCondition = listOf(VisibleCondition(null, null, null)),
+                visibleCondition = visibleCondition,
                 isBlank = true,
                 formTemplateId = null,
                 questionIndex = 0,
@@ -64,11 +68,12 @@ class FormRenderingActivityTest{
                 questionType = QuestionTypeEnum.STRING,
                 hasCommentAttached = false,
                 required = false,
-                languageVersions = listOf(QuestionLangVersion("test-language", "test-parent","Test $i",i,null))
+                languageVersions = listOf(QuestionLangVersion("test-language",
+                    "test-parent","Test $i",i,null))
             )
             val categoryQuestion = Question(
                 id = "C$i",
-                visibleCondition = listOf(VisibleCondition(null, null, null)),
+                visibleCondition = visibleCondition,
                 isBlank = true,
                 formTemplateId = null,
                 questionIndex = 0,
@@ -79,28 +84,37 @@ class FormRenderingActivityTest{
                 questionType = QuestionTypeEnum.CATEGORY,
                 hasCommentAttached = false,
                 required = false,
-                languageVersions = listOf(QuestionLangVersion("test-language", "test-parent","Category #$i",i,null))
+                languageVersions = listOf(QuestionLangVersion("test-language",
+                    "test-parent","Category #$i",i,null))
             )
             questions.add(stringQuestion)
             questions.add(categoryQuestion)
         }
 
+        // Starts the activity with the given questions
         startActivityWithQuestions(questions)
 
+        // Returns the current activity of the UI
         // Source: https://stackoverflow.com/a/58684943
         fun getCurrentActivity(): Activity? {
             var currentActivity: Activity? = null
-            getInstrumentation().runOnMainSync { run { currentActivity = ActivityLifecycleMonitorRegistry.getInstance().getActivitiesInStage(
+            getInstrumentation().runOnMainSync { run { currentActivity =
+                ActivityLifecycleMonitorRegistry.getInstance().getActivitiesInStage(
                 Stage.RESUMED).elementAtOrNull(0) } }
             return currentActivity
         }
 
+        // Takes the name of a category, clicks its button and verifies that the correct name
+        // is visible in the title
         fun testCategoryButton(categoryName : String){
             onView(withId(R.id.form_state_button)).perform(click())
-            onView(withText(categoryName)).perform(scrollTo()).check(matches(isDisplayed())).perform(click())
+            onView(withText(categoryName)).perform(scrollTo()).check(matches(isDisplayed()))
+                .perform(click())
             assert(getCurrentActivity()?.title.toString() == categoryName)
         }
 
+        // Fill the answers to 9 questions and check that all the questions are appearing correctly.
+        // And test the category button for categories 1-9
         for (i in 1..9) {
             onView(withId(R.id.tv_question)).check(matches(withText("1. Test $i")))
             onView(withId(R.id.et_answer)).perform(typeText("Answer $i"))
@@ -108,12 +122,32 @@ class FormRenderingActivityTest{
             testCategoryButton("Category #$i")
         }
 
+        // Test uncategorized button and verify that the answer persists after switching categories
         testCategoryButton("Uncategorized")
         onView(withId(R.id.et_answer)).check(matches(withText("Answer 1")))
 
+        // Verify that there is no Category 10
         onView(withId(R.id.form_state_button)).perform(click())
         onView(withText("Category #10")).check(doesNotExist())
 
+        // Verify that the next button moves categories appropriately
+        for (i in 1..9) {
+            onView(withId(R.id.form_next_category_button)).perform(click())
+            assert(getCurrentActivity()?.title.toString() == "Category #$i")
+        }
+        onView(withId(R.id.form_next_category_button)).perform(click())
+        assert(getCurrentActivity()?.title.toString() == "Category #9")
+
+        // Verify that the prev button moves categories appropriately
+        for (i in 9 downTo 1) {
+            assert(getCurrentActivity()?.title.toString() == "Category #$i")
+            onView(withId(R.id.form_prev_category_button)).perform(click())
+        }
+        assert(getCurrentActivity()?.title.toString() == "Uncategorized")
+        onView(withId(R.id.form_prev_category_button)).perform(click())
+        assert(getCurrentActivity()?.title.toString() == "Uncategorized")
+
+        // Verify that submit options appear correctly
         onView(withId(R.id.formSubmit)).perform(click())
         onView(withText("SMS")).perform(scrollTo()).check(matches(isDisplayed()))
         onView(withText("Wifi")).perform(scrollTo()).check(matches(isDisplayed()))
