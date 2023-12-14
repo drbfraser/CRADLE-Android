99ec7f9d505aa3711f8bb9183cc5d578cf2fd314
Peter <cfa60@sfu.ca>
2023-12-13 20:41:03 -0800

    Added Exception handling and comments.

--- app/src/main/java/com/cradleplatform/neptune/http_sms_service/sms/utils/SMSDataProcessor.kt
+++ app/src/main/java/com/cradleplatform/neptune/http_sms_service/sms/utils/SMSDataProcessor.kt
@@ -13,86 +13,135 @@ import com.google.gson.Gson
 import javax.inject.Inject
 import javax.inject.Singleton
 
+/**
+ * Utility class to transform Data objects into a JSON String that can be read by Cradle Backend's
+ * SMS API Endpoint after being relayed and decrypted, which will forward to other internal API
+ * Endpoints.
+ */
 @Singleton
 class SMSDataProcessor @Inject constructor(private val urlManager: UrlManager) {
     // TODO: Add target API endpoint information needed by the backend to json ??
     // TODO: requestNumber=0 as it is not implemented in the backend yet
-    fun processFormToJSON(formResponse: FormResponse): String {
-        val formResponseJSON = Gson().toJson(formResponse)
-        val url = Uri.parse(urlManager.uploadFormResponse)
-        val endpoint = url.path
-        return JacksonMapper.createWriter<SmsReadingWithReferral>().writeValueAsString(
-            SmsReadingWithReferral(
-                requestNumber = "0",
-                method = "POST",
-                endpoint = endpoint!!,
-                headers = "",
-                body = formResponseJSON
-            )
-        )
-    }
-    fun processPatientAndReadingsToJSON(patientAndReadings: PatientAndReadings): String {
-        if (patientAndReadings.patient.lastServerUpdate == null) {
-            val patientAndReadingsJSON = JacksonMapper.createWriter<PatientAndReadings>().writeValueAsString(
-                patientAndReadings
-            )
-            val url = Uri.parse(urlManager.postPatient)
-            val endpoint = url.path
-            return JacksonMapper.createWriter<SmsReadingWithReferral>().writeValueAsString(
-                SmsReadingWithReferral(
-                    requestNumber = "0",
-                    method = "POST",
-                    endpoint = endpoint!!,
-                    headers = "",
-                    body = patientAndReadingsJSON
-                )
-            )
-        } else {
-            val readingJSON = JacksonMapper.createWriter<Reading>().writeValueAsString(
-                patientAndReadings.readings[0])
 
-            val url = Uri.parse(urlManager.postReading)
-            val endpoint = url.path
+    /**
+     * Transforms a FormResponse into a Cradle Server SMS API compatible JSON String.
+     * @param formResponse
+     * @return A JSON formatted String, else empty String if an Exception is caught.
+     */
+    fun processFormToJSON(formResponse: FormResponse): String {
+        try {
+            val formResponseJSON = Gson().toJson(formResponse)
+            val url = Uri.parse(urlManager.uploadFormResponse)
+            val endpoint = url.path ?: throw Exception("URL path is null")
             return JacksonMapper.createWriter<SmsReadingWithReferral>().writeValueAsString(
                 SmsReadingWithReferral(
                     requestNumber = "0",
                     method = "POST",
-                    endpoint = endpoint!!,
+                    endpoint = endpoint,
                     headers = "",
-                    body = readingJSON
+                    body = formResponseJSON
                 )
             )
+        } catch (e: Exception) {
+            Error(e.message, e.cause)
+            return ""
         }
     }
-    fun processPatientAndReferralToJSON(patientAndReferrals: PatientAndReferrals): String {
-        val patientAndReferralsJSON = JacksonMapper.createWriter<PatientAndReferrals>()
-            .writeValueAsString(patientAndReferrals)
-        val url = Uri.parse(urlManager.postPatient)
-        val endpoint = url.path
-        return JacksonMapper.createWriter<SmsReadingWithReferral>().writeValueAsString(
-            SmsReadingWithReferral(
-                requestNumber = "0",
-                method = "POST",
-                endpoint = endpoint!!,
-                headers = "",
-                body = patientAndReferralsJSON
-            )
-        )
+
+    /**
+     * Transforms a PatientAndReadings into a Cradle Server SMS API compatible JSON String.
+     * @param patientAndReadings
+     * @return A JSON formatted String, else empty String if an Exception is caught.
+     */
+    fun processPatientAndReadingsToJSON(patientAndReadings: PatientAndReadings): String {
+        try {
+            if (patientAndReadings.patient.lastServerUpdate == null) {
+                val patientAndReadingsJSON = JacksonMapper.createWriter<PatientAndReadings>().writeValueAsString(
+                    patientAndReadings
+                )
+                val url = Uri.parse(urlManager.postPatient)
+                val endpoint = url.path ?: throw Exception("URL path is null")
+                return JacksonMapper.createWriter<SmsReadingWithReferral>().writeValueAsString(
+                    SmsReadingWithReferral(
+                        requestNumber = "0",
+                        method = "POST",
+                        endpoint = endpoint,
+                        headers = "",
+                        body = patientAndReadingsJSON
+                    )
+                )
+            } else {
+                val readingJSON = JacksonMapper.createWriter<Reading>().writeValueAsString(
+                    patientAndReadings.readings[0])
+                val url = Uri.parse(urlManager.postReading)
+                val endpoint = url.path ?: throw Exception("URL path is null")
+                return JacksonMapper.createWriter<SmsReadingWithReferral>().writeValueAsString(
+                    SmsReadingWithReferral(
+                        requestNumber = "0",
+                        method = "POST",
+                        endpoint = endpoint,
+                        headers = "",
+                        body = readingJSON
+                    )
+                )
+            }
+        } catch (e: Exception) {
+            Error(e.message, e.cause)
+            return ""
+        }
     }
-    fun processReferralToJSON(referral: Referral): String {
-        val referralJSON = JacksonMapper.createWriter<Referral>().writeValueAsString(
-            referral
-        )
-        val url = Uri.parse(urlManager.postReferral)
-        val endpoint = url.path
-        return JacksonMapper.createWriter<SmsReadingWithReferral>().writeValueAsString(
-            SmsReadingWithReferral(
-                requestNumber = "0",
-                method = "POST",
-                endpoint = endpoint!!,
-                headers = "",
-                body = referralJSON
+
+    /**
+     * Transforms a PatientAndReferrals into a Cradle Server SMS API compatible JSON String.
+     * @param patientAndReferrals
+     * @return A JSON formatted String, else empty String if an Exception is caught.
+     */
+    fun processPatientAndReferralToJSON(patientAndReferrals: PatientAndReferrals): String {
+        try {
+            val patientAndReferralsJSON = JacksonMapper.createWriter<PatientAndReferrals>()
+                .writeValueAsString(patientAndReferrals)
+            val url = Uri.parse(urlManager.postPatient)
+            val endpoint = url.path ?: throw Exception("URL path is null")
+            return JacksonMapper.createWriter<SmsReadingWithReferral>().writeValueAsString(
+                SmsReadingWithReferral(
+                    requestNumber = "0",
+                    method = "POST",
+                    endpoint = endpoint,
+                    headers = "",
+                    body = patientAndReferralsJSON
+                )
             )
-        )
+        } catch (e: Exception) {
+            Error(e.message, e.cause)
+            return ""
+        }
+    }
+
+
+    /**
+     * Transforms a Referral into a Cradle Server SMS API compatible JSON String.
+     * @param referral
+     * @return A JSON formatted String, else empty String if an Exception is caught.
+     */
+    fun processReferralToJSON(referral: Referral): String {
+        try {
+            val referralJSON = JacksonMapper.createWriter<Referral>().writeValueAsString(
+                referral
+            )
+            val url = Uri.parse(urlManager.postReferral)
+            val endpoint = url.path ?: throw Exception("URL path is null")
+            return JacksonMapper.createWriter<SmsReadingWithReferral>().writeValueAsString(
+                SmsReadingWithReferral(
+                    requestNumber = "0",
+                    method = "POST",
+                    endpoint = endpoint,
+                    headers = "",
+                    body = referralJSON
+                )
+            )
+        } catch (e: Exception) {
+            Error(e.message, e.cause)
+            return ""
+        }
     }
 }
