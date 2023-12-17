4346575562fec93086c2db0604af0ea0207e0c1f
Ekam Hothi <ehothi@sfu.ca>
2023-12-16 16:25:32 -0800

    Removed unused imports

--- app/src/main/java/com/cradleplatform/neptune/view/PatientReferralActivity.kt
+++ app/src/main/java/com/cradleplatform/neptune/view/PatientReferralActivity.kt
@@ -5,9 +5,7 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingComponent
--- app/src/main/java/com/cradleplatform/neptune/view/ui/reading/ReferralDialogFragment.kt
+++ app/src/main/java/com/cradleplatform/neptune/view/ui/reading/ReferralDialogFragment.kt
@@ -10,7 +10,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageButton
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingComponent
