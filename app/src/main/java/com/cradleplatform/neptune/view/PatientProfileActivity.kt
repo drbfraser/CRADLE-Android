b995ce5b96db8ebc46d9a668e53769af133501b2
DINA ZENG <dhz1@sfu.ca>
2024-02-24 06:19:28 -0800

    resolved 14 warnings

--- app/src/main/java/com/cradleplatform/neptune/view/PatientProfileActivity.kt
+++ app/src/main/java/com/cradleplatform/neptune/view/PatientProfileActivity.kt
@@ -7,7 +7,6 @@ import android.content.SharedPreferences
 import android.os.Build
 import android.os.Bundle
 import android.view.Menu
-import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.widget.Button
@@ -16,7 +15,6 @@ import android.widget.LinearLayout
 import android.widget.RadioGroup
 import android.widget.TextView
 import android.widget.Toast
-import androidx.annotation.Nullable
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
@@ -50,8 +48,6 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import java.text.ParseException
-import java.util.ArrayList
-import java.util.Comparator
 import javax.inject.Inject
 
 /**
@@ -62,6 +58,7 @@ import javax.inject.Inject
  * The usage of runBlocking is not ideal for performance.
  */
 
+@Suppress("DEPRECATION")
 @AndroidEntryPoint
 open class PatientProfileActivity : AppCompatActivity() {
 
@@ -106,12 +103,6 @@ open class PatientProfileActivity : AppCompatActivity() {
         private const val EXTRA_PATIENT = "patient"
         private const val EXTRA_PATIENT_ID = "patientId"
 
-        fun makeIntentForPatient(context: Context, patient: Patient): Intent {
-            val intent = Intent(context, PatientProfileActivity::class.java)
-            intent.putExtra(EXTRA_PATIENT, patient)
-            return intent
-        }
-
         fun makeIntentForPatientId(context: Context, patientId: String): Intent {
             val intent = Intent(context, PatientProfileActivity::class.java)
             intent.putExtra(EXTRA_PATIENT_ID, patientId)
@@ -143,19 +134,21 @@ open class PatientProfileActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
+        return when (item.itemId) {
             R.id.create_referral -> {
                 Toast.makeText(this, "Creating a new Referral", Toast.LENGTH_SHORT).show()
                 createNewReferral()
-                return true
+                true
             }
+
             R.id.create_form -> {
                 Toast.makeText(this, "Creating a new Form", Toast.LENGTH_SHORT).show()
                 createNewForm()
-                return true
+                true
             }
+
             else -> {
-                return super.onOptionsItemSelected(item)
+                super.onOptionsItemSelected(item)
             }
         }
     }
@@ -286,7 +279,7 @@ open class PatientProfileActivity : AppCompatActivity() {
     }
 
     private fun setupBtnPregnancy(patient: Patient) {
-        btnPregnancy.setOnClickListener() {
+        btnPregnancy.setOnClickListener {
 
             // Don't allow users to end a pregnancy when they haven't synced the start date
             if (patient.pregnancyId == null && patient.gestationalAge != null) {
@@ -307,7 +300,7 @@ open class PatientProfileActivity : AppCompatActivity() {
 
     private fun setupEditPatient(patient: Patient) {
         val editIcon = findViewById<ImageView>(R.id.im_edit_personal_info)
-        editIcon.setOnClickListener() {
+        editIcon.setOnClickListener {
             val intent = EditPatientInfoActivity.makeIntentWithPatientId(this, patient.id)
             startActivity(intent)
         }
@@ -319,7 +312,7 @@ open class PatientProfileActivity : AppCompatActivity() {
      *
      * @param patient current patient
      */
-    fun setupGestationalInfo(patient: Patient) {
+    private fun setupGestationalInfo(patient: Patient) {
         val radioGroup = findViewById<RadioGroup>(R.id.gestationradioGroup)
         radioGroup.setOnCheckedChangeListener { _: RadioGroup?, index: Int ->
             val ageVal: Double? = if (index == R.id.monthradiobutton) {
@@ -566,7 +559,7 @@ open class PatientProfileActivity : AppCompatActivity() {
     override fun onActivityResult(
         requestCode: Int,
         resultCode: Int,
-        @Nullable data: Intent?
+        data: Intent?
     ) {
         if (requestCode == READING_ACTIVITY_DONE) {
             updateUi()
@@ -595,7 +588,7 @@ open class PatientProfileActivity : AppCompatActivity() {
     override fun onBackPressed() {
         super.onBackPressed()
 
-        var intent = PatientsActivity.makeIntent(this@PatientProfileActivity)
+        val intent = PatientsActivity.makeIntent(this@PatientProfileActivity)
         startActivity(intent)
     }
 }
