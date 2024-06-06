0d1f62833fa40db23870497603abc70a9156debc
DINA ZENG <dhz1@sfu.ca>
2024-06-05 19:53:43 -0700

    set up forms functionality

--- app/src/main/java/com/cradleplatform/neptune/view/DashBoardActivity.kt
+++ app/src/main/java/com/cradleplatform/neptune/view/DashBoardActivity.kt
@@ -229,6 +229,13 @@ class DashBoardActivity : AppCompatActivity(), View.OnClickListener {
         val helpImg = helpLayout.findViewById<ImageButton>(R.id.educationImg)
         helpCardView.setOnClickListener(this)
         helpImg.setOnClickListener(this)
+
+        val formsLayout =
+            findViewById<View>(R.id.formsConstraintLayout)
+        val formsCardView: CardView = formsLayout.findViewById(R.id.formsCardView)
+        val formsImg = formsLayout.findViewById<ImageButton>(R.id.formsImg)
+        formsCardView.setOnClickListener(this)
+        formsImg.setOnClickListener(this)
     }
 
     private fun checkPinIfPinSet() {
@@ -271,19 +278,14 @@ class DashBoardActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View) {
         when (v.id) {
-            R.id.readingCardView, R.id.readingImg -> {
-                val intent = ReadingActivity.makeIntentForNewReading(this@DashBoardActivity)
+            R.id.readingCardView, R.id.readingImg -> { val intent = ReadingActivity.makeIntentForNewReading(this@DashBoardActivity)
                 startActivityForResult(intent, READING_ACTIVITY_DONE)
             }
             R.id.patientCardView, R.id.patientImg -> startActivity(PatientsActivity.makeIntent(this))
             R.id.syncCardView, R.id.syncImg -> startActivity(Intent(this, SyncActivity::class.java))
             R.id.educationCardView, R.id.educationImg -> startActivity(Intent(this, EducationActivity::class.java))
-            R.id.statCardView, R.id.statImg -> startActivity(
-                Intent(
-                    this,
-                    StatsActivity::class.java
-                )
-            )
+            R.id.statCardView, R.id.statImg -> startActivity(Intent(this, StatsActivity::class.java))
+            R.id.formsCardView, R.id.formsImg -> startActivity(Intent(this, FormsActivity::class.java))
         }
     }
 
--- app/src/main/java/com/cradleplatform/neptune/view/FormsActivity.kt
+++ app/src/main/java/com/cradleplatform/neptune/view/FormsActivity.kt
@@ -1,4 +1,26 @@
 package com.cradleplatform.neptune.view
 
-class FormsActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cradleplatform.neptune.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class FormsActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_saved_forms)
+
+        setUpSavedFormsRecyclerView()
+        setUpActionBar()
+    }
+
+    private fun setUpSavedFormsRecyclerView() {
+
+    }
+    private fun setUpActionBar() {
+        supportActionBar?.title = getString(R.string.see_saved_forms)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
 }
