23c2a66959b7eea4093dd89e507d1a52e2eeacd3
lcchow <lcchow@sfu.ca>
2025-03-15 10:42:01 -0700

    fix: fixes for messaging and better UI appearance

--- app/src/main/java/com/cradleplatform/neptune/http_sms_service/sms/ui/SmsTransmissionDialogViewModel.kt
+++ app/src/main/java/com/cradleplatform/neptune/http_sms_service/sms/ui/SmsTransmissionDialogViewModel.kt
@@ -1,6 +1,7 @@
 package com.cradleplatform.neptune.http_sms_service.sms.ui
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.map
 import com.cradleplatform.neptune.http_sms_service.sms.SMSSender
@@ -18,19 +19,48 @@ class SmsTransmissionDialogViewModel @Inject constructor(
     val smsSender: SMSSender
 ) : ViewModel() {
 
-    val stateString: LiveData<String> = smsStateReporter.state.map {
-        when (it) {
+    val stateString = MediatorLiveData<String>(). apply {
+        addSource(smsStateReporter.state) { setStateString() }
+        addSource(smsStateReporter.statusCode) { setStateString() }
+    }
+
+    private fun MediatorLiveData<String>.setStateString() {
+        val state = smsStateReporter.state.value
+        val statusCode = smsStateReporter.statusCode.value
+
+        this.value = when (state) {
             SmsTransmissionStates.GETTING_READY_TO_SEND -> "Queuing SMS to be sent..."
             SmsTransmissionStates.SENDING_TO_RELAY_SERVER -> "Sending..."
             SmsTransmissionStates.WAITING_FOR_SERVER_RESPONSE -> "Waiting for confirmation..."
             SmsTransmissionStates.RECEIVING_SERVER_RESPONSE -> "Receiving confirmation..."
             SmsTransmissionStates.DONE -> "Processing, please wait..."
             SmsTransmissionStates.EXCEPTION -> "Handling error, exiting soon..."
-            SmsTransmissionStates.TIME_OUT -> "Timed out, no response"
-            SmsTransmissionStates.WAITING_FOR_USER_RESPONSE -> "Please confirm"
+            SmsTransmissionStates.TIME_OUT -> "Timed out, no response."
+            SmsTransmissionStates.WAITING_FOR_USER_RESPONSE -> {
+                when (statusCode) {
+                    200 -> "Success! Please confirm."
+                    in 400..599 -> "Error occurred, please review."
+                    else -> "Please confirm."
+                }
+            }
             else -> "Unknown state"
         }
     }
+
+//    val stateString: LiveData<String> = smsStateReporter.state.map {
+//        when (it) {
+//            SmsTransmissionStates.GETTING_READY_TO_SEND -> "Queuing SMS to be sent..."
+//            SmsTransmissionStates.SENDING_TO_RELAY_SERVER -> "Sending..."
+//            SmsTransmissionStates.WAITING_FOR_SERVER_RESPONSE -> "Waiting for confirmation..."
+//            SmsTransmissionStates.RECEIVING_SERVER_RESPONSE -> "Receiving confirmation..."
+//            SmsTransmissionStates.DONE -> "Processing, please wait..."
+//            SmsTransmissionStates.EXCEPTION -> "Handling error, exiting soon..."
+//            SmsTransmissionStates.TIME_OUT -> "Timed out, no response"
+//            SmsTransmissionStates.WAITING_FOR_USER_RESPONSE -> "Please confirm"
+//            else -> "Unknown state"
+//        }
+//    }
+
     val sendProgress: LiveData<String> = smsStateReporter.totalSent.map {
         val numerator = it.toString()
         val denominator = smsStateReporter.totalToBeSent.toString()
