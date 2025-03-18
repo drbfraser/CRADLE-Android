a94c88fe7af2392a9b7e55bffbcaa6e303b7c0e1
Ryan Milligan <rcm9@sfu.ca>
2025-03-18 09:05:52 -0700

    test: fix login test

--- app/src/androidTest/java/com/cradleplatform/neptune/view/LoginActivityUiTests.kt
+++ app/src/androidTest/java/com/cradleplatform/neptune/view/LoginActivityUiTests.kt
@@ -1,8 +1,6 @@
 package com.cradleplatform.neptune.view
 
-import android.content.Context
-import android.content.SharedPreferences
-import androidx.preference.PreferenceManager
+
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.IdlingResource
@@ -23,52 +21,29 @@ import androidx.test.rule.GrantPermissionRule
 import com.cradleplatform.neptune.R
 import com.cradleplatform.neptune.activities.authentication.LoginActivity
 import com.cradleplatform.neptune.activities.dashboard.DashBoardActivity
-import com.cradleplatform.neptune.activities.introduction.IntroActivity.Companion.LAST_VERSION_TO_COMPLETE_WIZARD
-import com.cradleplatform.neptune.di.SharedPreferencesModule
+import com.cradleplatform.neptune.testutils.WorkManagerRule
 import com.cradleplatform.neptune.testutils.grantPermissions
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.HiltAndroidRule
-import dagger.hilt.android.testing.UninstallModules
-import dagger.hilt.components.SingletonComponent
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import javax.inject.Singleton
 
-@UninstallModules(SharedPreferencesModule::class)
 @HiltAndroidTest
 @LargeTest
 @RunWith(AndroidJUnit4::class)
 class LoginActivityUiTests {
-
-    @Module
-    @InstallIn(SingletonComponent::class)
-    class TestModule {
-        @Singleton
-        @Provides
-        fun providesSharedPreferences(@ApplicationContext context: Context): SharedPreferences {
-            /** To avoid the "Grant Permissions" intro screen, we need to set
-             * `LAST_VERSION_TO_COMPLETE_WIZARD` in shared preferences.
-             * We can create a new SharedPreferences to inject so as to avoid polluting the
-             * default SharedPreferences. */
-            val sharedPreferences = context.getSharedPreferences("ui-test", Context.MODE_PRIVATE)
-            sharedPreferences.edit().putLong(LAST_VERSION_TO_COMPLETE_WIZARD, 1)
-            return sharedPreferences
-        }
-    }
-
     // https://developer.android.com/training/dependency-injection/hilt-testing#ui-test
     @get:Rule(order = 0)
     var hiltRule = HiltAndroidRule(this)
 
-    // https://developer.android.com/guide/components/activities/testing
     @get:Rule(order = 1)
+    var workManagerRule = WorkManagerRule()
+
+    // https://developer.android.com/guide/components/activities/testing
+    @get:Rule(order = 2)
     var activityScenarioRule = activityScenarioRule<LoginActivity>()
 
     private lateinit var idlingResource: IdlingResource
@@ -80,6 +55,7 @@ class LoginActivityUiTests {
 
     @Before
     fun before() {
+        hiltRule.inject()
         Intents.init()
         activityScenarioRule.scenario.onActivity { activity ->
             idlingResource = activity.getIdlingResource()
@@ -122,6 +98,7 @@ class LoginActivityUiTests {
             .perform(pressImeActionButton())
 
         onView(withId(R.id.passwordEditText))
+            .perform(click())
             .perform(typeTextIntoFocusedView("this password has to be long enough"))
             .perform(pressImeActionButton())
 
