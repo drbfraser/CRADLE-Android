e59a1c05d53079e2400623018eb2d5c9fcf8c431
Ryan Milligan <rcm9@sfu.ca>
2025-03-26 15:42:50 -0700

    refactor: remove unused import

--- app/src/main/java/com/cradleplatform/neptune/activities/authentication/PinPassActivity.kt
+++ app/src/main/java/com/cradleplatform/neptune/activities/authentication/PinPassActivity.kt
@@ -1,6 +1,5 @@
 package com.cradleplatform.neptune.activities.authentication
 
-import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
